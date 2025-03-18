8c57a8531c361f4a78ea5e37aea4fb930c5d0969
Anthony Chapman <adc@thewebdev.us>
2025-03-18 13:07:09 -0400

    task: default to one example

--- src/tools/design_retriever.py
+++ src/tools/design_retriever.py
@@ -2,7 +2,7 @@ from nodes.design_rag import DesignRAG
 from langgraph.graph import MessagesState
 from langchain_core.messages import SystemMessage
 
-async def design_retriever_tool(state: MessagesState, num_examples: int = 2):
+async def design_retriever_tool(state: MessagesState, num_examples: int = 1):
     """
         Retrieves similar designs based on style requirements
         Name: query_similar_designs
